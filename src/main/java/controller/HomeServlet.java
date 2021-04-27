b6f508f1c731c37c9e4c2ef807c2a321382ba3d9
luigi-crescenzo7 <l.crescenzo14@studenti.unisa.it>
2021-04-27 13:47:22 +0200

    added ss.jsp and modified some classes

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -1,33 +1,42 @@
 package controller;
 
 import model.Utente;
+import model.UtenteDAO;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @WebServlet(name = "HomeServlet", urlPatterns = "/home")
 public class HomeServlet extends HttpServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String fieldUsername = request.getParameter("fieldUsername");
+        String fieldEmail = request.getParameter("fieldEmail");
         String fieldPassword = request.getParameter("fieldPassword");
 
-        if (fieldPassword == null || fieldUsername == null)
+        if (fieldPassword == null || fieldEmail == null)
             return;
 
-        if (fieldPassword.isEmpty() || fieldUsername.isEmpty())
+        if (fieldPassword.isEmpty() || fieldEmail.isEmpty())
             return;
 
         Utente user = new Utente();
+        UtenteDAO dao = new UtenteDAO();
+        user.setEmail(fieldEmail);
+        user.setPasswordhash(fieldPassword);
 
+        dao.doSave(user);
+
+        request.getRequestDispatcher("/WEB-INF/ss.jsp").forward(request, response);
     }
 
     public void destroy() {
