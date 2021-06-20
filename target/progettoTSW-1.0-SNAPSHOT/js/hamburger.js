19f05daa3a16ac58bfd3c2cfe7b5fa80cf8d4b4f
luigi-crescenzo7 <l.crescenzo14@studenti.unisa.it>
2021-06-20 18:08:54 +0200

    some refactoring

--- target/progettoTSW-1.0-SNAPSHOT/js/hamburger.js
+++ target/progettoTSW-1.0-SNAPSHOT/js/hamburger.js
@@ -1,5 +1,12 @@
 const menu = document.querySelector("#img-menu")
 menu.addEventListener("click", function hor_lines_click() {
     let hamburger = document.querySelector(".hamburger_menu")
+    //let container = document.querySelector(".container")
+    //hamburger.classList.toggle("hamburger_menu_visible")
+    //hamburger.style.width = "250px"
+    //container.style.marginLeft = "250px"
+    let container = document.querySelector(".container")
     hamburger.classList.toggle("hamburger_menu_visible")
+    console.log(menuSize)
+
 })
